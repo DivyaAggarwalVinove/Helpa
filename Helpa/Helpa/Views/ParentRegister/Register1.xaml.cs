fde7a11bd36d47552ae2098cf68df67597b5b646
Divya Aggarwal <divya.aggarwal@mail.vinove.com>
2018-05-14 18:34:19 +0530

    Changes in Register1 for signup screen 1/2

--- Helpa/Helpa/Views/ParentRegister/Register1.xaml.cs
+++ Helpa/Helpa/Views/ParentRegister/Register1.xaml.cs
@@ -12,11 +12,26 @@ namespace Helpa
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Register1 : ContentPage
 	{
-		public Register1 ()
+        //RegisterViewModel1 vm = null;
+
+        public Register1 ()
 		{
 			InitializeComponent ();
 
             NavigationPage.SetHasNavigationBar(this, false);
+
+            //vm = new RegisterViewModel1();
+            //this.BindingContext = vm;
+            //MyRadioGroup.CheckedChanged += MyRadioGroup_CheckedChanged;
+            //this.MyRadioGroup.SelectedIndex = 3;
         }
-	}
+
+        //void MyRadioGroup_CheckedChanged(object sender, int e)
+        //{
+        //    var radio = sender as CustomRadioButton;
+        //    if (radio == null || radio.Id == -1) return;
+        //    //this.txtSelected.Text = radio.Text;
+        //    vm.SelectedIndex = this.MyRadioGroup.SelectedIndex;
+        //}
+    }
 }
